ccb1414047534972151542a861f34deb0e771280
Tis-Control-HA <tishomeassistant@gmail.com>
2026-05-18 12:09:57 +0300

    Remove console logs from WebSocket client
    Removed console log statements related to WebSocket events for cleaner output.

--- webrtc-voice-streamer/ui/js/websocket-client.js
+++ webrtc-voice-streamer/ui/js/websocket-client.js
@@ -12,18 +12,15 @@ export class SignalingClient {
     const wsPath = window.location.pathname.replace(/\/$/, "") + "/ws";
     const wsUrl = `${protocol}//${window.location.host}${wsPath}`;
 
-    console.log(`Attempting WebSocket connection to: ${wsUrl}`);
     this.ws = new WebSocket(wsUrl);
 
     this.ws.onopen = () => {
-      console.log("WebSocket connection opened successfully.");
       if (this.callbacks.onConnectionChange) {
         this.callbacks.onConnectionChange(true);
       }
     };
 
     this.ws.onclose = () => {
-      console.log("WebSocket connection closed. Attempting to reconnect in 3 seconds...");
       if (this.callbacks.onConnectionChange) {
         this.callbacks.onConnectionChange(false);
       }
@@ -35,38 +32,32 @@ export class SignalingClient {
     };
 
     this.ws.onmessage = async (e) => {
-      console.log("WebSocket message received:", e.data);
       try {
         const msg = JSON.parse(e.data);
 
         switch (msg.type) {
           case "sender_ready":
-            console.log("Sender ready received, creating WebRTC offer...");
             if (this.callbacks.onSenderReady) this.callbacks.onSenderReady();
             break;
 
           case "webrtc_answer":
-            console.log("Received WebRTC answer. Setting remote description...");
             if (this.callbacks.onAnswer) this.callbacks.onAnswer(msg.answer);
             break;
 
           case "available_streams":
             this.activeStreams = msg.streams || [];
-            console.log("Available streams updated:", this.activeStreams);
             if (this.callbacks.onStreamsUpdated) this.callbacks.onStreamsUpdated(this.activeStreams);
             break;
 
           case "stream_available":
             if (!this.activeStreams.includes(msg.stream_id)) {
               this.activeStreams.push(msg.stream_id);
-              console.log("Stream available:", msg.stream_id, "Active streams:", this.activeStreams);
               if (this.callbacks.onStreamsUpdated) this.callbacks.onStreamsUpdated(this.activeStreams);
             }
             break;
 
           case "stream_ended":
             this.activeStreams = this.activeStreams.filter((id) => id !== msg.stream_id);
-            console.log("Stream ended:", msg.stream_id, "Active streams:", this.activeStreams);
             if (this.callbacks.onStreamsUpdated) this.callbacks.onStreamsUpdated(this.activeStreams);
             break;
 
@@ -75,7 +66,6 @@ export class SignalingClient {
             break;
 
           default:
-            console.log("Received unknown message type:", msg.type);
             break;
         }
       } catch (err) {
@@ -86,21 +76,18 @@ export class SignalingClient {
 
   sendStartSending() {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      console.log("Sending start_sending command over WebSocket.");
       this.ws.send(JSON.stringify({ type: "start_sending" }));
     }
   }
 
   sendStopStream() {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      console.log("Sending stop_stream command over WebSocket.");
       this.ws.send(JSON.stringify({ type: "stop_stream" }));
     }
   }
 
   sendOffer(offer) {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      console.log("Sending WebRTC offer over WebSocket...");
       this.ws.send(JSON.stringify({ type: "webrtc_offer", offer }));
     }
   }
